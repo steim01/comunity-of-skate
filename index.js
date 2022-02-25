5f862f1f4f03db5e27e09d3bc56c3ec5f6fac537
steim01 <dev.steim01@gmail.com>
2022-02-24 23:25:36 -0300

    usando um pouco mais de gri

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const botaoMenu = document.querySelector('.cabecalho__menu')
 const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--ativo')
+    menu.classList.toggle('menu-lateral--ativo');
 })
